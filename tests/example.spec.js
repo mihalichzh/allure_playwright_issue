6e33c4c42ed99828e9e5f33aece7533fb0dcf7b2
Mikhail Zheltyshev <mihalich93@bk.ru>
2023-07-20 22:31:19 +0200

    added project example for reproduction

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,8 +1,8 @@
 const {test, expect} = require('@playwright/test');
 
-["one", "two"].forEach(word => {
+['one', 'two'].forEach(word => {
     test.describe(`parameterized describe - ${word}`, () => {
-        test(`input word`, async ({page}) => {
+        test('input word', async ({page}) => {
             await page.goto('https://www.google.com/');
 
             await page.locator('[name=q]').fill(word);
